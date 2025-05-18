6a3e0ebaed6c0d4d4754401fc08ad82e3a198261
Alexis Revol <revol.alexis.lgm@gmail.com>
2025-05-18 17:08:52 +0200

    Premiere page et component

---
+++ frontend/src/app/components/counter/counter.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-counter',
+  standalone: true,
+  templateUrl: './counter.component.html',
+  styleUrls: ['./counter.component.css']
+})
+export class CounterComponent {
+  count = 0;
+
+  increment() {
+    this.count++;
+  }
+}
---
+++ frontend/src/app/pages/home/home.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CounterComponent } from 'app/components/counter/counter.component';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [CounterComponent],
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent {
+  title = 'Bienvenue HomeComponent sur le suivi de candidatures';
+}
